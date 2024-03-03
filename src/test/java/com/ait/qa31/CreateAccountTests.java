16657edc191ed2428bebb5b4566b5d6a63d7b18b
Egor Knestiapin <knesp@Yandex.ru>
2024-03-03 22:44:28 +0400

    add dataProvider createAccount method

--- src/test/java/com/ait/qa31/CreateAccountTests.java
+++ src/test/java/com/ait/qa31/CreateAccountTests.java
@@ -2,7 +2,16 @@ package com.ait.qa31;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class CreateAccountTests extends TestBase {
 
@@ -18,6 +27,40 @@ public class CreateAccountTests extends TestBase {
         fillLoginRegisterForm(email, "Qwerty123$");
 
         type(By.id("ConfirmPassword"), "Qwerty123$");
+
+        clickOnTheRegisterButton();
+
+        Assert.assertTrue(isElementPresent(By.xpath(String.format("//a[text()='%s']", email))));
+    }
+
+    @DataProvider
+    public Iterator<Object[]> createAccount() {
+        List<Object[]> list = new ArrayList<>();
+        try (BufferedReader bf = new BufferedReader(new FileReader("src/test/resources/user.csv"))) {
+            String s = bf.readLine();
+            while (s != null) {
+                String[] split = s.split(";");
+                list.add(new Object[]{split[0], split[1], split[2], split[3], split[4]});
+                s = bf.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "createAccount")
+    public void createNewAccountPositiveTestFromCsv(String firstname, String lastname, String email, String password,
+                                                    String confirmPassword) {
+        clickOnTheRegisterLink();
+
+        type(By.id("FirstName"), firstname);
+
+        type(By.id("LastName"), lastname);
+
+        fillLoginRegisterForm(email, password);
+
+        type(By.id("ConfirmPassword"), confirmPassword);
 
         clickOnTheRegisterButton();
 
